058937e94ebb7ac77821be3c57b3a9529c3d4da0
Tyler Treat <ttreat31@gmail.com>
2013-02-01 23:36:58 -0600

    Changed sendData to send int counter.

--- src/com/digitalxyncing/client/DatagramClient.java
+++ src/com/digitalxyncing/client/DatagramClient.java
@@ -2,6 +2,7 @@ package com.digitalxyncing.client;
 
 import java.io.IOException;
 import java.net.*;
+import java.nio.ByteBuffer;
 
 public class DatagramClient {
 
@@ -20,15 +21,14 @@ public class DatagramClient {
             System.out.println("Attemping to connect to " + address + ") via UDP port 9876");
 
             byte[] sendData = new byte[1024];
-
-            String message = "This is a test!";
-            sendData = message.getBytes();
+            int val = 0;
+            sendData = ByteBuffer.allocate(4).putInt(val).array();
 
             DatagramPacket sendPacket = new DatagramPacket(sendData, sendData.length, address, PORT);
             while (true) {
                 clientSocket.send(sendPacket);
-                System.out.println("Data sent");
-                Thread.sleep(5);
+                Thread.sleep(1);
+                sendPacket.setData(ByteBuffer.allocate(4).putInt(++val).array());
             }
         } catch (SocketException e) {
             e.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
