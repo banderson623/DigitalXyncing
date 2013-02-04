ed0a23a9c1a6e055b5d68472935945ff92d75eb9
Brian Anderson <banderson623@gmail.com>
2013-02-04 11:34:52 -0600

    Starting to throw around interface ideas. Nothing serious, more a place holder for future interfaces

---
+++ src/com/digitalxyncing/interfaces/DocumentInterface.java
@@ -0,0 +1,22 @@
+package com.digitalxyncing.interfaces;
+
+/**
+ * User: brian_anderson
+ * Date: 2/4/13
+ * Just adding some ideas here. Please change as required.
+ * Add some readme here about how this operates
+ */
+public interface DocumentInterface<TypeOfDocument extends Object> {
+    public interface DocumentFragment<TypeOfDocument> {
+        public String toString();
+    }
+
+    public boolean update(DocumentFragment<TypeOfDocument> newFragment);
+    /**
+     *
+     * @return
+     */
+    public TypeOfDocument getFullState();
+
+
+}
