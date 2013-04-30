df28a018d149995ccad15b870b2ce643b72ece00
Brian Anderson <banderson623@gmail.com>
2013-04-29 22:27:36 -0500

    implementation of sniffy

---
+++ src/com/digitalxyncing/communication/impl/ZmqSniffy.java
@@ -0,0 +1,10 @@
+package com.digitalxyncing.communication.impl;
+
+/**
+ * User: brian_anderson
+ * Date: 4/29/13
+ * <p/>
+ * Add some readme here about how this operates
+ */
+public class ZmqSniffy {
+}
