960de76c1167aabb801274ca10430bc6b6a49141
Brian Anderson <banderson623@gmail.com>
2013-05-03 00:57:46 -0500

    Added more documentation to the Sniffy Interface

--- src/com/digitalxyncing/communication/Sniffy.java
+++ src/com/digitalxyncing/communication/Sniffy.java
@@ -3,10 +3,7 @@ package com.digitalxyncing.communication;
 import java.util.List;
 
 /**
- * User: brian_anderson
- * Date: 4/29/13
- * <p/>
- * Add some readme here about how this operates
+ * Created by Brian Anderson, see impl/ZmqSniffy.java for an implementation of this.
  */
 public interface Sniffy {
     /**
@@ -18,7 +15,24 @@ public interface Sniffy {
      */
     List<String> discoverXyncersOnPort(int portNumber);
 
-    void discoverXyncersOnMySubnetAndFromListOfIPsAndPort(final List<String> fullIpAddress, final int portNumber, final HostListCallBack callBack);
+    /**
+     * Will scan the local subnet, 10.0.0.* (or whatever the DX-node's current IP address is)
+     * and will call "callBack.results(...)" passing in a list of Strings mapping to discovered
+     * IP addresses
+     *
+     * @param fullIpAddress this is a list of full IP addresses. Each IP addresses' subnet
+     *                      will be scanned and discovered DX-nodes will be return in the results
+     *                      call back
+     * @param portNumber    This is the port number to scan on
+     * @param callBack      The Callback object's results() method will be called once all of the
+     *                      required network-nodes have been scanned. This list will include the
+     *                      IP addresses of all the Digital Xyncing nodes that are hosting a document.
+     */
+    void discoverXyncersOnMySubnetAndFromListOfIPsAndPort(final List<String> fullIpAddress,
+                                                          final int portNumber,
+                                                          final HostListCallBack callBack);
+
+
     void discoverXyncersOnPort(int portNumber, HostListCallBack callBack);
 
     public interface HostListCallBack {
